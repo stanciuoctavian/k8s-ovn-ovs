84af0b3abc9b3bee555513f12a9d7545e6e21e3b
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2019-02-08 19:24:17 +0200

    WIP: Flannel

--- v2/flannel.py
+++ v2/flannel.py
@@ -1,6 +1,10 @@
 import ovn_ovs
+import time
 
 class Flannel_CI(ovn_ovs.OVN_OVS_CI):
     def __init__(self):
         super(Flannel_CI, self).__init__()
-        print dir(self)
+    
+    def _prepare_ansible(self):
+        time.sleep(10000000)
+

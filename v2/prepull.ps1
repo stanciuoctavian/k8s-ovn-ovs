6a217048dea80145ac6c1415810c72d35f40e949
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2019-03-03 21:53:36 +0200

    Change prepull script

--- v2/prepull.ps1
+++ v2/prepull.ps1
@@ -1,9 +1,7 @@
 # Script file to create tmp directory in windows nodes
 
+mkdir c:\tmp
+
 Write-Host "Prepulling all test images"
 
-
-wget https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/images/Utils.ps1 -UseBasicParsing -OutFile Utils.ps1
-wget https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/images/PullImages.ps1 -UseBasicParsing -OutFile PullImages.ps1
-
-./PullImages.ps1
+docker pull e2eteam/busybox:1.29

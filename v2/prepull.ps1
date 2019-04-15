de2c75b6254a20a12da0243fa2a637e26ad88d80
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2019-04-15 13:42:59 +0300

    Set DOCKER_API_VERSION as 1.39

--- v2/prepull.ps1
+++ v2/prepull.ps1
@@ -5,3 +5,5 @@ mkdir c:\tmp
 Write-Host "Prepulling all test images"
 
 docker pull e2eteam/busybox:1.29
+
+[System.Environment]::SetEnvironmentVariable('DOCKER_API_VERSION', "1.39", [System.EnvironmentVariableTarget]::Machine)

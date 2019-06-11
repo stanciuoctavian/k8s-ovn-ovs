196e55398d9ed22799d5769adbae6a27e5cafafd
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2019-06-11 15:46:16 +0300

    Update prepull.ps1 script to be runtime aware.

--- v2/prepull.ps1
+++ v2/prepull.ps1
@@ -1,9 +1,21 @@
 # Script file to create tmp directory in windows nodes
 
+param (
+    [ValidateSet("docker","containerd")][string]$runtime = "docker"
+)
+
 mkdir c:\tmp
+
+$pullCmd = "docker pull"
+if ( $runtime -eq "containerd") {
+    $pullCmd = "c:\k\ctr.exe --namespace k8s.io image pull"
+}
+
+Write-Host "Disable monitoring"
+Set-MpPreference -DisableRealtimeMonitoring $true
 
 Write-Host "Prepulling all test images"
 
-docker pull e2eteam/busybox:1.29
+iex "$pullCmd docker.io/e2eteam/busybox:1.29"
 
 [System.Environment]::SetEnvironmentVariable('DOCKER_API_VERSION', "1.39", [System.EnvironmentVariableTarget]::Machine)
